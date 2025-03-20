e1c6c1d5e6f2b931a3cfbb5dee43308b97a19b2d
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-03-20 17:12:50 -0400

    fix: Update styles of sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,16 +15,14 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = ({ isSidebarVisible }) => {
   const [activeMenu, setActiveMenu] = useState('dashboard');
-  //cerrar secion
-    const handleLogout = () => {
+
+  const handleLogout = () => {
     signOut();
   };
 
   const handleMenuClick = (menu) => {
     setActiveMenu(menu);
   };
-
-
 
   return (
     <section id="sidebar" className={!isSidebarVisible ? 'hide' : ''}>
@@ -34,31 +32,31 @@ const Sidebar = ({ isSidebarVisible }) => {
       </Link>
       <ul className="side-menu top">
         <li className={activeMenu === 'dashboard' ? 'active' : ''}>
-          <Link to="/home" onClick={() => handleMenuClick('dashboard')}> 
+          <Link to="/home" className="link" onClick={() => handleMenuClick('dashboard')}>
             <FaThLarge className="icon" />
             <span className="text">Dashboard</span>
           </Link>
         </li>
         <li className={activeMenu === 'store' ? 'active' : ''}>
-          <Link to="/inicio" onClick={() => handleMenuClick('store')}>
+          <Link to="/inicio" className="link" onClick={() => handleMenuClick('store')}>
             <FaShoppingBag className="icon" />
             <span className="text">My Store</span>
           </Link>
         </li>
         <li className={activeMenu === 'analytics' ? 'active' : ''}>
-          <Link to="/home" onClick={() => handleMenuClick('analytics')}> 
+          <Link to="/home" className="link" onClick={() => handleMenuClick('analytics')}>
             <FaChartPie className="icon" />
             <span className="text">Analytics</span>
           </Link>
         </li>
         <li className={activeMenu === 'message' ? 'active' : ''}>
-          <Link to="/message" onClick={() => handleMenuClick('message')}> 
+          <Link to="/message" className="link" onClick={() => handleMenuClick('message')}>
             <FaCommentDots className="icon" />
             <span className="text">Message</span>
           </Link>
         </li>
         <li className={activeMenu === 'team' ? 'active' : ''}>
-          <Link to="/team" onClick={() => handleMenuClick('team')}> 
+          <Link to="/team" className="link" onClick={() => handleMenuClick('team')}>
             <FaUsers className="icon" />
             <span className="text">Team</span>
           </Link>
@@ -66,17 +64,16 @@ const Sidebar = ({ isSidebarVisible }) => {
       </ul>
       <ul className="side-menu">
         <li>
-          <Link to="/settings"> 
+          <Link to="/settings" className="link">
             <FaCog className="icon" />
             <span className="text">Settings</span>
           </Link>
         </li>
         <li>
-            <button onClick={handleLogout} className='logout'>
-                <FaSignOutAlt className="icon" />
-                <span className="text">Logout</span>
-            </button>
-          
+          <button onClick={handleLogout} className="logout">
+            <FaSignOutAlt className="icon" />
+            <span className="text">Logout</span>
+          </button>
         </li>
       </ul>
     </section>
