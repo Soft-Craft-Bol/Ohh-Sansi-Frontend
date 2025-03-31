6bc7b9ca1ce337f6e76b570aeb4489c58c7cf0f9
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-03-30 21:09:09 -0400

    fix: Update forms step2 with areas by id grade

--- src/components/selected/SelectInput.jsx
+++ src/components/selected/SelectInput.jsx
@@ -3,34 +3,33 @@ import { useField } from "formik";
 import "./SelectInput.css";
 
 function SelectInput({ label, required, options, loading, emptyMessage, ...props }) {
-  const [field, meta] = useField(props);
+    const [field, meta] = useField(props);
 
-  return (
-    <div className="select-component">
-      <label htmlFor={props.id || props.name}>
-        {label} {required && <span className="required">*</span>}
-      </label>
-      <select className="select-input" {...field} {...props}>
-        <option value="">Seleccione una opción</option>
-        {loading ? (
-  <option disabled>Cargando...</option>
-) : !Array.isArray(options) || options.length === 0 ? (
-  <option disabled>{emptyMessage || "No hay opciones disponibles"}</option>
-) : (
-  options.map((option) => (
-    <option key={option.value} value={option.value}>
-      {option.label}
-    </option>
-  ))
-)}
+    return (
+        <div className="select-component">
+            <label htmlFor={props.id || props.name}>
+                {label} {required && <span className="required">*</span>}
+            </label>
+            <select className="select-input" {...field} {...props}>
+                <option value="">Seleccione una opción</option>
+                {loading ? (
+                    <option disabled>Cargando...</option>
+                ) : !Array.isArray(options) || options.length === 0 ? (
+                    <option disabled>{emptyMessage || "No hay opciones disponibles"}</option>
+                ) : (
+                    options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))
+                )}
 
-
-      </select>
-      {meta.touched && meta.error ? (
-        <div className="error-message">{meta.error}</div>
-      ) : null}
-    </div>
-  );
+            </select>
+            {meta.touched && meta.error ? (
+                <div className="error-message">{meta.error}</div>
+            ) : null}
+        </div>
+    );
 }
 
 export default SelectInput;
