2e64ce3a5169e35af970c5c18daf4130e8429d33
Dylanzeb <zeballosperedodylanamin@gmail.com>
2025-04-15 21:52:18 -0400

    Fix: maxlength

--- src/components/inputs/InputText.jsx
+++ src/components/inputs/InputText.jsx
@@ -3,7 +3,7 @@ import { useField } from "formik";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./InputText.css";
 
-function InputText({ label, required, type = "text", as = "input", showCounter = false, ...props }) {
+function InputText({ label, required, type = "text", as = "input", showCounter = false, maxLength, ...props}) {
     const [field, meta, helpers] = useField(props);
     const [showPassword, setShowPassword] = useState(false);
 
