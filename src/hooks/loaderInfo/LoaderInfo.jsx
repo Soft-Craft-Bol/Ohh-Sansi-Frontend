a68c365760a88509c81209d3d1d3d8cf581d1350
jhoelito123 <amercer732@gmail.com>
2025-04-21 16:51:55 -0400

    Feat: function verifyTutor

--- src/hooks/loaderInfo/LoaderInfo.jsx
+++ src/hooks/loaderInfo/LoaderInfo.jsx
@@ -40,3 +40,81 @@ export const verificarParticipante = async (ci, onComplete, onError) => {
     onError?.("No se pudo verificar la información.");
   }
 };
+
+export const verificarSerTutor = async (ci, onSuccess, onError) => {
+  if (!ci) return;
+
+  try {
+    const res = await getEstudianteByCarnet(ci);
+
+    if (res.data?.nombres && res.data?.apellidos) {
+      const nombre = res.data.nombres.split(" ")[0];
+      const apellidos = res.data.apellidos.split(" ");
+      const apellidoInicial = apellidos.length > 0 ? apellidos[0][0] + "." : "";
+
+      const { value: emailConfirm } = await Swal.fire({
+        title: `¿Conoces a ${nombre} ${apellidoInicial}?`,
+        text: "Si es así, ingresa su correo para continuar como tutor",
+        input: "email",
+        inputPlaceholder: "correo@ejemplo.com",
+        confirmButtonText: "Confirmar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        inputValidator: (value) => {
+          if (!value) return "Debes ingresar un correo válido.";
+        },
+      });
+
+      if (!emailConfirm) {
+        return;
+      }
+
+      const result = await verifyEstudiante({ ci, valuePermit: emailConfirm });
+
+      if (result.data) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+
+        Toast.fire({
+          icon: "success",
+          title: "Verificación exitosa"
+        });
+
+        onSuccess?.(result.data); // Devuelve los datos al componente
+      }
+
+    } else {
+      throw new Error("Participante no encontrado");
+    }
+  } catch (error) {
+    console.error("Error verificando como tutor:", error);
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+
+    Toast.fire({
+      icon: "error",
+      title: "No se pudo verificar el CI"
+    });
+
+    onError?.(); // Por ejemplo para limpiar el campo
+  }
+};
