e642b958f4ba8a3c863717e6d10878a5cdaccc06
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-04-30 22:53:49 -0400

    fix: update bugs

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { lazy} from "react";
+import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "../context/PrivateRoute";
 import Layout from "../Layout/Layout";
@@ -6,67 +6,89 @@ import Layout from "../Layout/Layout";
 // Importaciones Lazy
 const MainContent = lazy(() => import("../components/sidebar/LandingPage"));
 const OrdenDePago = lazy(() => import("../pages/ordenDePago/OrdenDePago"));
-const MultiStepForm = lazy(() => import("../components/multiStepForm/MultiStepForm"));
+const MultiStepForm = lazy(
+  () => import("../components/multiStepForm/MultiStepForm")
+);
 const ManagementPage = lazy(() => import("../pages/admin/Management"));
-const FormArea = lazy(() => import("../components/management/formArea/FormArea"));
-const NotFound404 = lazy(() => import("../pages/404NotFound/NotFound404").then(module => ({ default: module.NotFound404 })));
-const EstadoDeInscripcion = lazy(() => import('../pages/estadoDeInscripcion/EstadoDeInscripcion'));
+const FormArea = lazy(
+  () => import("../components/management/formArea/FormArea")
+);
+const NotFound404 = lazy(() =>
+  import("../pages/404NotFound/NotFound404").then((module) => ({
+    default: module.NotFound404,
+  }))
+);
+const EstadoDeInscripcion = lazy(
+  () => import("../pages/estadoDeInscripcion/EstadoDeInscripcion")
+);
 const LoginUser = lazy(() => import("../pages/login/LoginUser"));
-const InscripcionExcel = lazy(() => import('../pages/inscripcion/InscripcionExcel'));
-
+const InscripcionExcel = lazy(
+  () => import("../pages/inscripcion/InscripcionExcel")
+);
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Ruta pública */}
-      <Route path="/login" element={<LoginUser />} />
-      <Route path="/*" element={<NotFound404 />} />
-    
-
-     
-      <Route path="/" element={
-        <Layout>
+      <Route
+        path="/login"
+        element={<LoginUser />}
+      />
+      <Route
+        path="/*"
+        element={<NotFound404 />}
+      />
+      <Route
+        path="/"
+        element={
+          <Layout>
             <MainContent />
-        </Layout>
-      }/>
-
-
-      <Route path="/inscripcion-individual" element={
-        <Layout>
+          </Layout>
+        }
+      />
+      <Route
+        path="/inscripcion-individual"
+        element={
+          <Layout>
             <MultiStepForm />
-        </Layout>
-      }/>
-
+          </Layout>
+        }
+      />
       <Route
         path="/inscripcion-masiva"
         element={
           <Layout>
-              <InscripcionExcel/>
+            <InscripcionExcel />
+          </Layout>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <ManagementPage />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/orden-de-pago"
+        element={
+          <Layout>
+            <OrdenDePago />
           </Layout>
         }
       />
 
-      <Route path="/admin" element={
-        <PrivateRoute>
+      <Route
+        path="/estado-de-inscripcion"
+        element={
           <Layout>
-              <ManagementPage />
-          </Layout>
-        </PrivateRoute>
-      }/>
-
-
-      <Route path="/orden-de-pago" element={
-        <Layout>
-            <OrdenDePago />
-        </Layout>
-      }/>
-
-      <Route path="/estado-de-inscripcion" element={
-        <Layout>
             <EstadoDeInscripcion />
-        </Layout>
-      }/>
-
+          </Layout>
+        }
+      />
     </Routes>
   );
 };
